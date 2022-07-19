af3cbf9e5a3ab8a9a819a6fdc95904a2244c48be
maxmay94 <maxmay94@gmail.com>
2022-07-19 10:17:41 -0400

    change and style nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,12 +4,11 @@ const NavBar = ({ user, handleLogout }) => {
   return (
     <>
       {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
+        <nav className='bg-slate-800 font-semibold text-white'>
+          <ul className='flex p-3'>
+            <li className='flex-1 ml-3'>Welcome, {user.name}</li>
+            <li className='flex-1 ml-16'><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+            <li className='mr-3'><Link to="/changePassword">Change Password</Link></li>
           </ul>
         </nav>
       :
