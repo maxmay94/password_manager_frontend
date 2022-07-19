993c54d949b82cd8ac95c6ea9bafefd3fbd08220
maxmay94 <maxmay94@gmail.com>
2022-07-19 11:17:01 -0400

    update dependencies

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,8 @@
 const Landing = ({ user }) => {
   return (
-    <main className='text-center'>
-      <h1 className='p-5 text-4xl font-thin text-slate-300'>{user ? 'Hello ' + user.name + '.' : 'Please log in or create an account to continue...'}</h1>
+    <main className='text-center bg-slate-900/20'>
+      <h1 className='pt-5 text-4xl font-thin text-slate-300'>{user ? 'Hello ' + user.name + '.': 'Please log in or create an account to continue...'}</h1>
+      <h1 className='pb-5 text-3xl font-thin text-amber-500'>{user && 'Your secrets are safe.'}</h1>
     </main>
   )
 }
