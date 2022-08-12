eb3b7818cf31862a5cf6da0bc25ec77975be99ee
maxmay94 <maxmay94@gmail.com>
2022-08-12 10:49:16 -0400

    update responsivness on landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = ({ user, passwords }) => {
       <h1 className='pt-5 text-4xl font-thin text-slate-300'>{user ? 'hello ' + user.name + '.': 'please log in or create an account to continue...'}</h1>
       <h1 className='pb-5 text-3xl font-thin text-amber-500'>{user && 'your secrets are safe.'}</h1>
 
-      <div className="px-40">
+      <div className="px-5 sm:px-20 md:px-30 lg:px-40">
         {
           user&&
             passwords&&
@@ -13,7 +13,7 @@ const Landing = ({ user, passwords }) => {
               {
                 passwords?.map((password, i) => (
                   <div key={password._id} className='text-center m-1'>
-                    <div className={ i % 2 === 0 ? 'bg-blue-500 text-black py-3 hover:bg-slate-200 rounded-sm': 'bg-blue-900 py-3 hover:bg-slate-200 hover:text-slate-900 rounded-sm'}>
+                    <div className={ i % 2 === 0 ? 'bg-blue-500 text-black py-3 hover:bg-slate-200 rounded-sm' : 'bg-blue-900 py-3 hover:bg-slate-200 hover:text-slate-900 rounded-sm'}>
                       {password.site} {password.passW} {password.createdAt}
                     </div>
                   </div>
